b445369b215bb285253263a3f697e1fa6d7a9d31
Amy Kang <amykang@Amys-MacBook-Air.local>
2017-05-05 22:57:32 -0700

    Initial Commit

---
+++ Categories/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Categories
+//
+//  Created by Amy Kang on 5/5/17.
+//  Copyright © 2017 Amy Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
