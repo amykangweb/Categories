148004d52cde3a61922fbc999432d3d43fa2733b
Amy Kang <amykang@Amys-MacBook-Air.local>
2017-05-05 23:36:58 -0700

    Pointers in practice.

--- Categories/main.m
+++ Categories/main.m
@@ -9,11 +9,34 @@
 #import <Foundation/Foundation.h>
 #import "NSArray+Movies.h"
 
+// int increment (int x);
+void increment (int *x);
+
 int main(int argc, const char * argv[]) {
 
     NSArray *rawArray = @[@"Gravity", @"The Martian", @"Interstellar", @"The Martian", @"Gravity", @"Guardians of the Galaxy"];
     
     rawArray = [rawArray dedupe];
     
+    /*
+    NSString *fruit = @"apple";
+    NSLog(@"fruit value: %@ \n", fruit);
+    NSLog(@"fruit add: %p \n", &fruit);
+     */
+    
+    int i = 5;
+    NSLog(@"i value: %i \n", i);
+    NSLog(@"i addy: %p \n", &i);
+    
+    increment(&i);
+    NSLog(@"i value: %i", i);
+    NSLog(@"i addy: %p \n", &i);
+    
     return 0;
+}
+
+void increment (int *x) {
+    *x = *x + 1;
+    NSLog(@"x val: %i \n", *x);
+    NSLog(@"x add: %p \n", x);
 }
